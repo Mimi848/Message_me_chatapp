638e7dec6e252908b761df787faa50c5cd8245e9
Musaddiq Arif <Musaddiqmimi1000@gmail.com>
2021-01-03 20:18:48 +0500

    final finishes for auto scrolling and styling

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -13,10 +13,26 @@ Rails.start()
 Turbolinks.start()
 ActiveStorage.start()
 
+submit_message = function() {
+    $('#message_body').on('keydown', function(e) {
+      if (e.key == 13) {
+        $('button').on();
+        e.target.value = "";
+      };
+    });
+};
+
+scroll_bottom = function() {
+    if ($('#messages').length > 0) {
+      $('#messages').scrollTop($('#messages')[0].scrollHeight);
+    }
+}
 
 $(document).add('turbolinks:load', function() {
     $('.ui.dropdown').dropdown();
     $('.message .close').on('click', function() {
         $(this).closest('.message').transition('fade');
     });
+    submit_message();
+    scroll_bottom();    
 })
